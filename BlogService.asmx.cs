f06055cca2d8dd4c8dbf45aa5fadae6efd245519
熊胡超 <18373463@buaa.edu.cn>
2020-11-18 16:23:31 +0800

    添加项目文件。

---
+++ BlogService.asmx.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace BlogService
+{
+    /// <summary>
+    /// BlogService 的摘要说明
+    /// </summary>
+    [WebService(Namespace = "http://bearhuchao.top/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // 若要允许使用 ASP.NET AJAX 从脚本中调用此 Web 服务，请取消注释以下行。 
+    // [System.Web.Script.Services.ScriptService]
+    public class BlogService : System.Web.Services.WebService
+    {
+        readonly WebServiceDbContext DB = new WebServiceDbContext();
+        [WebMethod]
+        public string AddUser(string username)
+        {
+            var targetUser = DB.Users.Where(x => x.Username == username).FirstOrDefault();
+            if (targetUser != null)
+            {
+                return targetUser.Id;
+            }
+            User newUser = new User { Username = username, Blogs = new List<Blog>() };
+            DB.Users.Add(newUser);
+            try
+            {
+                DB.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                return e.ToString();
+            }
+            return newUser.Id;
+        }
+        [WebMethod]
+        public UserInfo GetUser(string username)
+        {
+            var targetUser = DB.Users.Where(x => x.Username == username).FirstOrDefault();
+            if (targetUser == null)
+            {
+                return new UserInfo();
+            }
+            return new UserInfo() { Id = targetUser.Id, Username = targetUser.Username, Blogs = targetUser.Blogs.Count };
+        }
+
+        [WebMethod]
+        public UserInfo[] GetUsers()
+        {
+            return DB.Users.Select(user => new UserInfo()
+            {
+                Id = user.Id,
+                Username = user.Username,
+                Blogs = user.Blogs.Count
+            }).ToArray();
+        }
+
+        [WebMethod]
+        public string AddBlog(string title, string author, string content)
+        {
+            var targetUser = DB.Users.Where(x => x.Username == author).FirstOrDefault();
+            if (targetUser == null)
+            {
+                var newUserId = AddUser(author);
+                targetUser = DB.Users.Find(newUserId);
+            }
+            Blog newBlog = new Blog
+            {
+                Title = title,
+                Author = targetUser,
+                Content = content,
+                Comments = new List<Comment>()
+            };
+            targetUser.Blogs.Add(newBlog);
+            DB.Blogs.Add(newBlog);
+            try
+            {
+                DB.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                return e.ToString();
+            }
+            return newBlog.Id;
+        }
+
+        [WebMethod]
+        public BlogInfo[] GetBlogs(string id)
+        {
+            return DB.Blogs.Where(x => x.Author.Id == id)
+                .Select(blog => new BlogInfo()
+                {
+                    Id = blog.Id,
+                    Title = blog.Title,
+                    CommentNumber = blog.Comments.Count
+                }).ToArray();
+        }
+
+        [WebMethod]
+        public BlogDetail GetBlogDetail(string id)
+        {
+            return DB.Blogs.Where(x => x.Id == id)
+                .Select(blog => new BlogDetail
+                {
+                    Id = blog.Id,
+                    Title = blog.Title,
+                    Author = blog.Author.Username,
+                    UpdateTime = blog.CreatedAt.ToString(),
+                    Content = blog.Content
+                }).FirstOrDefault();
+        }
+        [WebMethod]
+        public CommentInfo[] GetBlogComments(string BlogId)
+        {
+            var blog = DB.Blogs.Find(BlogId);
+            return blog == null ? null : blog.Comments.Select(x => new CommentInfo() { Author = x.Author.Username, Content = x.Content }).ToArray();
+        }
+
+        [WebMethod]
+        public string AddBlogComment(string BlogId, string author, string content)
+        {
+            var targetUser = DB.Users.Where(x => x.Username == author).FirstOrDefault();
+            if (targetUser == null)
+            {
+                var newUserId = AddUser(author);
+                targetUser = DB.Users.Find(newUserId);
+            }
+            var targetBlog = DB.Blogs.Find(BlogId);
+            if (targetBlog == null)
+            {
+                return null;
+            }
+            Comment newComment = new Comment
+            {
+                Author = targetUser,
+                Blog = targetBlog,
+                Content = content
+            };
+            DB.Comments.Add(newComment);
+            try
+            {
+                DB.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                return e.ToString();
+            }
+            return newComment.Id;
+        }
+    }
+}
---
+++ Models.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Data.Entity.Core.Common;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Data.SQLite;
+using System.Data.SQLite.EF6;
+using System.Linq;
+using System.Web;
+
+namespace BlogService
+{
+    public class BaseModel
+    {
+        [Key]
+        public string Id { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+
+        public BaseModel()
+        {
+            Id = Guid.NewGuid().ToString();
+            CreatedAt = DateTime.Now;
+            UpdatedAt = DateTime.Now;
+        }
+    }
+
+    public class User : BaseModel
+    {
+        public string Username { get; set; }
+
+        public virtual ICollection<Blog> Blogs { get; set; }
+    }
+
+    public class UserInfo
+    {
+        public string Id { get; set; }
+        public string Username { get; set; }
+
+        public int Blogs { get; set; }
+
+        public UserInfo()
+        {
+            Username = null;
+            Blogs = -1;
+        }
+    }
+
+
+    public class Blog : BaseModel
+    {
+        public virtual User Author { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public virtual IList<Comment> Comments { get; set; }
+    }
+    public class BlogInfo
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public int CommentNumber { get; set; }
+
+        public BlogInfo()
+        {
+            Id = null;
+            Title = null;
+            CommentNumber = -1;
+        }
+
+        public BlogInfo(Blog blog)
+        {
+            Title = blog.Title;
+            CommentNumber = blog.Comments.Count;
+        }
+    }
+
+    public class BlogDetail
+    {
+        public string Id { get; set; }
+        public string Author { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public string UpdateTime { get; set; }
+        public CommentInfo[] Comments { get; set; }
+
+        public BlogDetail() { }
+
+        public BlogDetail(Blog blog)
+        {
+            Author = blog.Author.Username;
+            Title = blog.Title;
+            Content = blog.Content;
+            CommentInfo[] CommentInfos = blog.Comments.Select(x => new CommentInfo(x)).ToArray();
+            Comments = CommentInfos;
+        }
+    }
+
+    public class Comment : BaseModel
+    {
+        public virtual User Author { get; set; }
+        public virtual Blog Blog { get; set; }
+        public string Content { get; set; }
+    }
+
+    public class CommentInfo
+    {
+        public string Author { get; set; }
+        public string Content { get; set; }
+
+        public CommentInfo()
+        {
+        }
+
+        public CommentInfo(Comment comment)
+        {
+            Author = comment.Author.Username;
+            Content = comment.Content;
+        }
+    }
+
+    public class WebServiceDbContext : DbContext
+    {
+
+        public WebServiceDbContext() :
+            base(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\database.mdf;Integrated Security=True;Connect Timeout=30")
+        {
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Comment>()
+                .HasRequired(x => x.Blog)
+                .WithMany(x => x.Comments)
+                .WillCascadeOnDelete();
+        }
+
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+
+        public override int SaveChanges()
+        {
+            var entries = ChangeTracker.Entries().ToList();
+            var updateEntries = entries.Where(e => (e.Entity is BaseModel)
+            && e.State == EntityState.Modified).ToList();
+
+            updateEntries.ForEach(e =>
+            {
+                ((BaseModel)e.Entity).UpdatedAt = DateTime.Now;
+            });
+            return base.SaveChanges();
+        }
+    }
+}
